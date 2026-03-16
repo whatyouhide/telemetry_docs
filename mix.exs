28c24bc70e9a155758ce200e6816c3948f836b76
Andrea Leopardi <an.leopardi@gmail.com>
2026-03-16 13:05:17 +0100

    Add GitHub CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TelemetryDocs.MixProject do
     [
       app: :telemetry_docs,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
