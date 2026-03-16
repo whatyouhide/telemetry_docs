b878ce452bb94040c594c1fbfa9210b2a48fb001
Andrea Leopardi <an.leopardi@gmail.com>
2026-03-16 13:15:42 +0100

    Add Hex info

--- mix.exs
+++ mix.exs
@@ -1,16 +1,30 @@
 defmodule TelemetryDocs.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @repo_url "https://github.com/whatyouhide/telemetry_docs"
+
   def project do
     [
       app: :telemetry_docs,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+
+      # Tests
       test_coverage: [tool: ExCoveralls],
+
+      # Hex
+      package: package(),
+      description:
+        "Generates Markdown documentation from structured telemetry event definitions.",
+
+      # Docs
       docs: [
-        main: "TelemetryDocs"
+        main: "TelemetryDocs",
+        source_ref: "v#{@version}",
+        source_url: @repo_url
       ]
     ]
   end
@@ -39,6 +53,14 @@ defmodule TelemetryDocs.MixProject do
       {:nimble_options, "~> 1.0"},
       {:ex_doc, "~> 0.40", only: :dev},
       {:excoveralls, "~> 0.18.5", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Andrea Leopardi"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url, "Sponsor" => "https://github.com/sponsors/whatyouhide"}
     ]
   end
 end
